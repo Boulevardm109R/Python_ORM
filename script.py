d1ab2bbe100686a0627538352df36821186bd1ff
Rostislav <99gruvdv@gmail.com>
2023-07-18 08:18:54 +0300

    does it done?

--- script.py
+++ script.py
@@ -17,26 +17,22 @@ engine = create_engine(f"postgresql://{db_username}:{db_password}@{db_host}/{db_
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Input publisher name or identifier
-publisher_name = input("Enter the name or identifier of the publisher: ")
 
-# Get the publisher by name or identifier
-publisher = session.query(Publisher).filter(Publisher.name == publisher_name).first()
-
-if publisher:
-    # Execute the query
-    query = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).\
-        join(Stock, Book.id == Stock.id_book).\
-        join(Sale, Stock.id == Sale.id_stock).\
-        join(Shop, Stock.id_shop == Shop.id).\
-        filter(Book.id_publisher == publisher.id)
-
-    # Iterate over the query results and print purchase facts
+def get_shops(publisher_name):
+    publisher = session.query(Publisher).filter(Publisher.name == publisher_name).first()
+    if publisher:
+        query = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).\
+            join(Stock, Book.id == Stock.id_book).\
+            join(Sale, Stock.id == Sale.id_stock).\
+            join(Shop, Stock.id_shop == Shop.id).\
+            filter(Book.id_publisher == publisher.id)
     for title, shop_name, price, date_sale in query:
         print(f"{title} | {shop_name} | {price} | {date_sale.strftime('%d-%m-%Y')}")
-
-else:
-    print("Publisher not found.")
+    else:
+        print("Publisher not found.")
 
 # Close the session
 session.close()
+if __name__ == '__main__':
+    publisher_name = input("Enter the name or identifier of the publisher: ")
+    get_shops(publisher_name)
